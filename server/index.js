6139330538de70e29bc84e2e20a72b6e594d9985
Titus Wormer <tituswormer@gmail.com>
2018-03-01 19:30:25 +0100

    server: fix typos

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ module.exports = express()
   .set('view engine', 'ejs')
   .set('views', 'view')
   .use(express.static('static'))
-  // TODO: Serve the images in `db/images/:id` as `/images/:id`.
+  // TODO: Serve the images in `db/image` on `/image`.
   .get('/', all)
   /* TODO: Other HTTP methods. */
   // .post('/', add)
